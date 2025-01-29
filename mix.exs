49a0ed3876e6cfa529f879a3cdfd2c495ffdea55
Gordon Woolbert <89322028+gdwoolbert3@users.noreply.github.com>
2025-01-29 13:53:10 -0500

    Bump deps and update readme (#12)

--- mix.exs
+++ mix.exs
@@ -53,8 +53,8 @@ defmodule GenBatcher.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.34.2", only: :dev, runtime: false},
-      {:credo, "~> 1.7.10", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.36.1", only: :dev, runtime: false},
+      {:credo, "~> 1.7.11", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4.3", only: [:dev, :test], runtime: false},
       {:styler, "~> 1.2.1", only: [:dev, :test], runtime: false}
     ]
